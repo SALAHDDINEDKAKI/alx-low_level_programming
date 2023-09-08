b4dc198ecc85952cac99d6053d65fff92eb82a7e
SALAHDDINEDKAKI <salahdkaki1999@gmail.com>
2023-09-07 17:03:15 -0700

    task4

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,17 @@
+#!/bin/bash
+#include <stdio.h>
+
+	/*
+	 * puts as function
+	 *
+	 * not allowed to use printf
+	 *
+	 * return 0 => call the func (puts)
+	 *
+	 */
+
+	int main(void){
+
+	puts("\n"Programming is like building a multilingual puzzle");
+	return(0);
+}
