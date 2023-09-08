6aac5eb927fa0c300c95d1ed4c366820305de295
SALAHDDINEDKAKI <salahdkaki1999@gmail.com>
2023-09-07 17:08:26 -0700

    task4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,16 +1,16 @@
 #include <stdio.h>
 
-	/*
-	 * puts as function
-	 *
-	 * not allowed to use printf
-	 *
-	 * return 0 => call the func (puts)
-	 *
-	 */
+	/**
+ 	* puts as function
+ 	*
+ 	* not allowed to use printf
+ 	*
+ 	* return 0 => call the func (puts)
+ 	*
+	*/
 
 	int main(void){
 
-	puts("\n"Programming is like building a multilingual puzzle");
+	puts("\"Programming is like building a multilingual puzzle");
 	return(0);
 }
