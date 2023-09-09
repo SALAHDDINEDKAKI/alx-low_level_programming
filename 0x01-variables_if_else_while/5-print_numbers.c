0b0cf2d07f3b9cd0e0a70230f788297293dcddd1
SALAHDDINEDKAKI <salahdkaki1999@gmail.com>
2023-09-09 14:00:41 -0700

    task6

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - english letters in lowercase
+ *
+ * Return: always 0 (Success)
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 0; i < 10; i++)
+	{
+		printf("%d", i);
+	}
+	printf("\n");
+	return (0);
+}
