f5d04cf572b668ebffd31c21a4e8a8163394aa93
SALAHDDINEDKAKI <salahdkaki1999@gmail.com>
2023-09-09 14:02:05 -0700

    task7

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - english letters in lowercase
+ *
+ * Return: always 0 (Success)
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 0; i < 10; i++)
+	{
+		putchar(i + '0');
+	}
+	putchar('\n');
+	return (0);
+}
