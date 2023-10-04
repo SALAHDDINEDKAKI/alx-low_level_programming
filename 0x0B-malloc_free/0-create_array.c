7a505633ff162ff69b2debfbfe237d81f37d14c9
SALAHDDINEDKAKI <salahdkaki1999@gmail.com>
2023-10-04 15:49:27 -0700

    task0

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -10,5 +10,17 @@
   */
 char *create_array(unsigned int size, char c)
 {
-	unsigned int i;
+	unsigned int a;
+	char *array;
+
+	if (size == 0)
+		return (NULL);
+
+	array = (char *)malloc(size * sizeof(char));
+	if (array == NULL)
+		return (NULL);
+
+	for (a = 0; a < size; a++)
+		array[a] = c;
+	return (array);
 }
